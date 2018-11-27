f048d8e92e5d8534cefdbbf05cfff29a2eb5072a
PaulineLabaisse <pauline.labaisse@orange.fr>
2018-11-27 17:20:49 +0100

    Fix front on views

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -1,9 +1,31 @@
 class TemplateController < ApplicationController
 
-def blog
-end
 
 def index
 end
 
 end
+
+def about
+
+end
+
+def checkout
+end
+
+
+def shop_full
+end
+
+def shopping_cart
+end
+
+def single_product
+end
+
+def contact
+end
+
+def shop_filter
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
- 
+
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/', to: 'template#index'
-get '/blog', to: 'template#blog'
+get '/index', to: 'template#index'
+get '/about', to: 'template#about'
+get '/checkout', to: 'template#checkout'
+get '/my-account', to: 'template#my-account'
+get '/shop-full', to: 'template#shop-full'
+get '/shopping-cart', to: 'template#shopping-cart'
+get '/single-product', to: 'template#single-product'
+get '/shop-filter', to: 'template#shop-filter'
+get '/contact', to: 'template#contact'
+
 
 end
