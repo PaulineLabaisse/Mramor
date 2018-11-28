0bb289bd112d88fcec116c4091b76c94cbff9ab1
irabespodobna <penkovskaya.irina@gmail.com>
2018-11-28 13:51:12 +0100

    a bit of non-functional devise admin

---
+++ app/controllers/admin/item_controller.rb
@@ -0,0 +1,42 @@
+class Admin::ItemController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+    flash[:notice] = "Product added!"
+    # redirect_to admin_items_path
+  else
+    flash[:notice] = "Product can't be blank!"
+    # render 'admin/items/new'
+    end
+  end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    flash[:notice] = "Product updated!"
+    # redirect_to admin_items_path
+
+  end
+
+    def destroy
+      Item.destroy(params[:id])
+      flash[:notice] = "Product removed!"
+    end
+
+    private
+    def item_params
+      params.require(:item).permit(:title, :description, :price, :image_url)
+    end
+end
